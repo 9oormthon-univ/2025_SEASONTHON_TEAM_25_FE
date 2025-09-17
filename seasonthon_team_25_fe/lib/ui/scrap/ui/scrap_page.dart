9b2c146598fc3c1334f72d5b13fc80874cce4d13
cyb <yeongbeen0x00@gmail.com>
2025-09-18 02:04:48 +0900

    fix: 제거된 파일 반영

--- seasonthon_team_25_fe/lib/ui/scrap/ui/scrap_page.dart
+++ seasonthon_team_25_fe/lib/ui/scrap/ui/scrap_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lottie/lottie.dart';
 import 'package:seasonthon_team_25_fe/core/theme/colors.dart';
-import 'package:seasonthon_team_25_fe/gen/assets.gen.dart';
 
 class ScrapPage extends ConsumerStatefulWidget {
   const ScrapPage({super.key});
@@ -16,58 +14,7 @@ class _ScrapPageState extends ConsumerState<ScrapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.wt,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Lottie.asset(
-                  Assets.lottie.test2Confetti,
-                  repeat: true,
-                  animate: true,
-                  fit: BoxFit.contain,
-                  frameRate: FrameRate.max,
-                ),
-              ),
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Lottie.asset(
-                  Assets.lottie.appLogo,
-                  repeat: true,
-                  animate: true,
-                  fit: BoxFit.contain,
-                  frameRate: FrameRate.max,
-                ),
-              ),
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Lottie.asset(
-                  Assets.lottie.test2Loading,
-                  repeat: true,
-                  animate: true,
-                  fit: BoxFit.contain,
-                  frameRate: FrameRate.max,
-                ),
-              ),
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Lottie.asset(
-                  Assets.lottie.loadingReversed,
-                  repeat: true,
-                  animate: true,
-                  fit: BoxFit.contain,
-                  frameRate: FrameRate.max,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: Center(child: const Text("Plz...")),
     );
   }
 }
